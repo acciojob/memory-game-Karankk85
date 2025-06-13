9148780a12fdd60022f2a669a012ee3e7bfab3ab
Karankk85 <karkk8533@gmail.com>
2025-06-13 15:12:25 +0530

    Update index.js
    asasas

--- src/index.js
+++ src/index.js
@@ -5,3 +5,22 @@ import App from "./components/App";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./store/reducers/index";
+import MemoryGameContainer from "./container/MemoryGameContainer/MemoryGameContainer";
+
+const store = createStore(rootReducer);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <MemoryGameContainer />
+    </Provider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
